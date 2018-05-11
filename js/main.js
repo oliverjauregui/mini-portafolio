9596b9d22fd32bf2af75c79538b7d3306aeb10ba
Dante <dnte.jauregui@gmail.com>
2018-05-11 06:26:45 -0700

    add scrollmagic

--- js/main.js
+++ js/main.js
@@ -83,3 +83,24 @@ $(document).ready(function() {
 	});
 
 });
+
+
+var controller = new ScrollMagic.Controller();
+
+var Scene1Left = new ScrollMagic.Scene({
+	triggerElement: '#traslateLeft1'
+})
+.setClassToggle('#traslateLeft1', 'fadeInLeftActive')
+.addTo(controller);
+
+var Scene1Right = new ScrollMagic.Scene({
+	triggerElement: '#traslateRight1'
+})
+.setClassToggle('#traslateRight1', 'fadeInRightActive')
+.addTo(controller);
+
+var Scene2 = new ScrollMagic.Scene({
+	triggerElement: '#fadeIn'
+})
+.setClassToggle('#fadeIn', 'fadeInActive')
+.addTo(controller);
